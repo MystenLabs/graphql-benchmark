1c0b04a38f06d9440801c0e91b201616be6beecb
Will Yang <willyang@mystenlabs.com>
2024-04-11 12:41:54 -0400

    tests

---
+++ tests/parameterization.test.ts
@@ -0,0 +1,17 @@
+import { generateCombinations } from "../parameterization";
+
+test("generateCombinations generates expected number of power sets", () => {
+  const parameters = {
+    filter: {
+      function: [
+        "0x8d97f1cd6ac663735be08d1d2b6d02a159e711586461306ce60a2b7a6a565a9e::pyth",
+        "0x000000000000000000000000000000000000000000000000000000000000dee9::clob_v2",
+      ],
+    },
+    first: [10, 20, 30],
+    last: [10, 20, 30],
+  };
+
+  let combinations = generateCombinations(parameters, ["function"]);
+  expect(combinations.length).toBe(45);
+});
